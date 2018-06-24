df48f11c3bcb32c342c9cdc6ae763b98a95d5dcb
Omkar Shelar <omkarshelar24@gmail.com>
2018-06-24 18:17:42 +0530

    Added route for static files

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask , render_template
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+@app.route('/static/<path:path>')
+def send_statics(path):
+    return send_from_directory('static', path)
 
 if __name__=='__main__':
     app.run()
