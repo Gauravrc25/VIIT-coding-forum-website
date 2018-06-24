55ee7739c81e8af5730e6096a6de78cd913ae938
Chinnmay <sarvasvachaudhari55555@gmail.com>
2018-06-24 17:07:48 +0530

    tp commit try commit

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask , render_template
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return "Hello World"
+    return render_template('home.html')
 
 
-
-if __name__ == '__main__':
-        app.run()
+if __name__=='__main__':
+    app.run()
