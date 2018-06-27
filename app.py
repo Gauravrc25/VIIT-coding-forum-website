df02203a30dc86049c909921ac6b18d0cf99f52a
Chinnmay <sarvasvachaudhari55555@gmail.com>
2018-06-27 19:36:08 +0530

    updated routes for newly added pages

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/static/<path:path>')
 def send_statics(path):
     return send_from_directory('static', path)
