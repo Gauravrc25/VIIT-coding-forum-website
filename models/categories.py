2af17c9e824cf50a475ed0bef34562c329a729b6
Gauravrc25 <Gauravrchaudhari@gmail.com>
2018-07-07 18:39:14 +0530

    Models update 1

--- models/categories.py
+++ models/categories.py
@@ -1,14 +1,58 @@
-import os
+import os, psycopg2
 from urllib import parse
-import psycopg2
 
 parse.uses_netloc.append("postgres")
 url = parse.urlparse(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+def insert_category(category):
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    cid = 0
+    cur = conn.cursor()
+    category = category.lower()
+    try:
+        cur.execute("Select id from categories where category_title='{0}'".format(category))
+        data = cur.fetchone()
+        cid = data[0]
+        
+        if cur.rowcount == 0:
+            cur.execute("insert into categories(category_title) values('{0}')".format(category))
+            conn.commit()
+            #print("inserted")
+            cur.execute("select id from categories where category_title='{0}'".format(category))
+            data = cur.fetchone()
+            cid = data[0]
+        
+        else:
+            print("Category Exists already")
+        print(cid)
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+        
+
+def delete_category(category):
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    cur = conn.cursor()
+    
+    try:
+        cur.execute("delete from categories where category_title='{0}'".format(category))
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
