54dd61d61071818d409ed3d4676d471f879f19f9
Gauravrc25 <Gauravrchaudhari@gmail.com>
2018-07-09 12:08:31 +0530

    models update(post) 3

--- models/users.py
+++ models/users.py
@@ -4,22 +4,30 @@ from urllib import parse
 parse.uses_netloc.append("postgres")
 url = parse.urlparse(os.environ["DATABASE_URL"])
 
-def check_key(key,conn):
+def check_key(key):
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
     
 
     cur = conn.cursor()
     
     try:
         cur.execute("select id from post where key ='{0}'".format(key))
-        if(cur.rowcount) != 0:
-            return 0
+        if cur.rowcount != 0:
+            return True
         else :
-            return 1
+            return False
     except Exception as e:
         print(e)
         
     finally:
         cur.close()
+        conn.close()
 # email key type
 def new_post(email,typel,key):
     conn = psycopg2.connect(
@@ -37,15 +45,11 @@ def new_post(email,typel,key):
     typel = typel.lower()
     #key = "12345697"
     user_id = 0
-    if check_key(key,conn) == 0:
-        return True
-    else:
-        return False
     try:
         cur.execute("select id from users where username='{0}'".format(email))
         data = cur.fetchone()
-        while data is not None:
-            user_id = data[0]
+        #while data is not None:
+        user_id = data[0]
             
         if cur.rowcount == 0:
             cur.execute("insert into users(username) values('{0}')".format(email))
@@ -53,8 +57,8 @@ def new_post(email,typel,key):
             print("Inserted")
             cur.execute("select id from users where username='{0}'".format(email))
             data = cur.fetchone()
-            while data is not None:
-                user_id = data[0]
+            #while data is not None:
+            user_id = data[0]
         
         cur.execute("insert into post(type,key,isactive,user_id) values('{0}','{1}','t',{2})".format(typel,key,user_id))
         conn.commit();       
@@ -65,33 +69,5 @@ def new_post(email,typel,key):
         conn.close()
     #print("Inserted")
 
+#new_post()
 
-
-def toggle(key):
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
-    cur = conn.cursor()
-    
-    try :
-        cur.execute("select isactive from post where key='{0}'".format(key))
-        switch = cur.fetchone()
-        switch = switch[0]
-        #print(switch)
-        
-        if switch == True:
-            cur.execute("update post set isactive='false' where key='{0}'".format(key))
-            conn.commit();
-        else :
-            cur.execute("update post set isactive='true' where key='{0}'".format(key))
-            conn.commit();
-    except Exception as e:
-        print(e)
-    finally:
-        cur.close()
-        conn.close()    
-  
